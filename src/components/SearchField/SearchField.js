6c2ee12de3ff0284bca6aca628d44a1b8ebd48b8
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-22 02:20:24 +0530

    Now forming Branch 2

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -15,6 +15,64 @@ const SearchField = ({
   onSearchClicked = (search) => { },
 }) => {
 
+  /*function dropDownDisplay() {
+    console.log("YUVRAJ SINGH......", suggestedArray);
+    return suggestedArray.map((index) => (
+      <p
+        onClick={() => {
+          onSearchClicked(suggestedArray[index]);
+          setText(suggestedArray[index]);
+          // searchFunction();
+        }}
+      >{suggestedArray[index]}</p>
+    ))
+  }*/
+
+  function searchFunction() {
+    onSearchClicked(text);
+
+    fetchAPokemon(text)
+      .then((pokemon) => {
+        setPokemon(pokemon);
+        // setStatus("resolved");
+      })
+      .catch((error) => {
+        //process the error
+        // setStatus("error");
+        console.log("error");
+      });
+
+    let id_value = lget(
+      pokemon,
+      "id"
+    );
+
+    setPokemonID(id_value);   //SEMI WORKING......
+    const BASE_URL_STRING = "https://pokeapi.co/api/v2/";
+    const pokeMonSuggestionURL = `?limit=10&offset=${pokemonID}`;
+    console.log('HEYYY...', pokeMonSuggestionURL);
+    console.log(pokemonID);
+    setFetchingUrl(pokeMonSuggestionURL);
+    console.log('WASSUP POKEMONS...', fetchingUrl);
+
+    fetchAPokemon(pokeMonSuggestionURL)
+      .then((resultValue) => {
+        let answer = resultValue.results;
+
+        for (let i = 0; i < 10; i++) {
+          let answervalue = answer[i].name;
+          arr.push(answervalue);
+          setSuggestedArray([...arr, answervalue]); //SEMI WORKING......
+        }
+      })
+      .catch((error) => {
+        // setStatus("error");
+        console.log("error");
+      });
+
+    console.log("ASHHH", suggestedArray);
+  }
+
   const arr = [];
   const [text, setText] = useState("");
 
@@ -23,6 +81,16 @@ const SearchField = ({
   const [suggestedArray, setSuggestedArray] = useState([]);
 
   const [fetchingUrl, setFetchingUrl] = useState('');
+
+  useEffect(() => {
+    searchFunction();
+  }, [fetchingUrl])
+
+  // useEffect(() => {
+  //   onSearchClicked(suggestedArray);
+  //   setText(suggestedArray);
+  //   searchFunction();
+  // },[fetchingUrl])
 
   useEffect(() => {
     fetchAPokemon(text)
@@ -55,11 +123,12 @@ const SearchField = ({
       });
     setFetchingUrl(fetchingUrl);
     console.log("ASHHH", suggestedArray);
+    // searchFunction();
   }, [text])
 
 
 
-  console.log("B3NKIGUYERTGY7RYIT3YTEIOGQ.............",suggestedArray);
+  console.log("B3NKIGUYERTGY7RYIT3YTEIOGQ.............", suggestedArray);
   return (
     <div className={styles.SearchField}>
       <input
@@ -78,51 +147,8 @@ const SearchField = ({
         disabled={text === ""}
         className={styles.SearchButton}
         onClick={() => {
-          onSearchClicked(text);
 
-          fetchAPokemon(text)
-            .then((pokemon) => {
-              setPokemon(pokemon);
-              // setStatus("resolved");
-            })
-            .catch((error) => {
-              //process the error
-              // setStatus("error");
-              console.log("error");
-            });
-
-          let id_value = lget(
-            pokemon,
-            "id"
-          );
-
-          setPokemonID(id_value);   //SEMI WORKING......
-          const BASE_URL_STRING = "https://pokeapi.co/api/v2/";
-          // const pokeMonSuggestionURL = `${BASE_URL_STRING}pokemon?limit=10&offset=${pokemonID}`;
-          const pokeMonSuggestionURL = `?limit=10&offset=${pokemonID}`;
-          console.log('HEYYY...', pokeMonSuggestionURL);
-          console.log(pokemonID);
-          setFetchingUrl(pokeMonSuggestionURL);
-          console.log('WASSUP POKEMONS...', fetchingUrl);
-
-          fetchAPokemon(pokeMonSuggestionURL)
-            .then((resultValue) => {
-              let answer = resultValue.results;
-              
-              for (let i = 0; i < 10; i++) {
-                let answervalue = answer[i].name;
-                arr.push(answervalue);
-                // setSuggestedArray(answervalue); //SEMI WORKING......
-                setSuggestedArray([...arr, answervalue]); //SEMI WORKING......
-                // setSuggestedArray(arr);//SEMI WORKING......
-              }
-            })
-            .catch((error) => {
-              // setStatus("error");
-              console.log("error");
-            });
-
-          console.log("ASHHH", suggestedArray);
+          searchFunction()
 
         }
         }
@@ -133,67 +159,79 @@ const SearchField = ({
       <div class={styles.dropdown}>
         <button class={styles.dropbtn}>
           Suggest
-      {/* <i class="fa fa-caret-down"></i> */}
         </button>
         <div class={styles.dropdownContent}>
+
+          {/* {dropDownDisplay()} */}
+
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[0]);
               setText(suggestedArray[0]);
+              // searchFunction();
             }}
           >{suggestedArray[0]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[1]);
               setText(suggestedArray[1]);
+              // searchFunction();
             }}
           >{suggestedArray[1]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[2]);
               setText(suggestedArray[2]);
+              // searchFunction();
             }}
           >{suggestedArray[2]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[3]);
               setText(suggestedArray[3]);
+              // searchFunction();
             }}
           >{suggestedArray[3]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[4]);
               setText(suggestedArray[4]);
+              // searchFunction();
             }}
           >{suggestedArray[4]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[5]);
               setText(suggestedArray[5]);
+              // searchFunction();
             }}
           >{suggestedArray[5]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[6]);
               setText(suggestedArray[6]);
+              // searchFunction();
             }}
           >{suggestedArray[6]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[7]);
               setText(suggestedArray[7]);
+              // searchFunction();
             }}
           >{suggestedArray[7]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[8]);
               setText(suggestedArray[8]);
+              // searchFunction();
             }}
           >{suggestedArray[8]}</p>
           <p
             onClick={() => {
               onSearchClicked(suggestedArray[9]);
               setText(suggestedArray[9]);
+              // searchFunction();
             }}
           >{suggestedArray[9]}</p>
         </div>
@@ -203,25 +241,7 @@ const SearchField = ({
       {/* Absolutely working id snippet */}
       {/* <div>{pokemonID}</div> */}
 
-      {/* {suggestedArray.map((index) => {
-        <ol>
-          <li>{suggestedArray[index]}</li>
-        </ol>
-      })} */}
 
-      {/* <ol>
-        <li>{suggestedArray[0]}</li>
-        <li>{suggestedArray[1]}</li>
-        <li>{suggestedArray[2]}</li>
-        <li>{suggestedArray[3]}</li>
-        <li>{suggestedArray[4]}</li>
-        <li>{suggestedArray[5]}</li>
-        <li>{suggestedArray[6]}</li>
-        <li>{suggestedArray[7]}</li>
-        <li>{suggestedArray[8]}</li>
-        <li>{suggestedArray[9]}</li>
-      </ol> */}
-      {/* ABOVE ONE IS WORKING CODE... */}
     </div>
 
   );
